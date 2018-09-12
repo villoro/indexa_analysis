d13e9aedbe37c96dcc0f704e723ef36e21144a8d
Villoro <villoro7@gmail.com>
2018-09-12 19:32:27 +0200

    [NEW] Add checklists and plot

--- src/layout.py
+++ src/layout.py
@@ -7,6 +7,10 @@ import dash_html_components as html
 
 import constants as c
 
+PLOT_CONFIG = {
+    "displaylogo": False,
+    "modeBarButtonsToRemove": ["sendDataToCloud", "select2d", "lasso2d", "resetScale2d"]
+}
 
 def create_sidebar(elements):
     """
@@ -34,6 +38,19 @@ def create_sidebar(elements):
     return [_get_sidebar_elem(title, data) for title, data in elements]
 
 
+def create_body(datalist):
+    """
+        Creates an element for the body
+
+        Args:
+            datalist:   what to include in the body
+
+        Return:
+            html div with the element
+    """
+    return [html.Div(data, className="row", style=c.STYLE_DIV_CONTROL_IN_BODY) for data in datalist]
+
+
 def get_layout():
     """ Creates the dash layout """
 
@@ -47,19 +64,23 @@ def get_layout():
         html.Div(
             create_sidebar([
                 ("Size", dcc.Checklist(
-                    id="drop_size", options=[
+                    id="drop_size", values=["B"], options=[
                         {'label': 'menos de 10k', 'value': 'A'},
                         {'label': 'de 10k a 100k', 'value': 'B'},
                         {'label': 'más de 100k', 'value': 'C'}
                     ]
+                )),
+                ("Risk", dcc.Checklist(
+                    id="drop_size", values=[1, 5, 10],
+                    options=[{'label': x, 'value': x} for x in range(1, 11)]
                 )),
             ]), id="sidebar", style=c.STYLE_SIDEBAR
         ),
 
         # Body
         html.Div(
-            [
-
-            ], id="body", style=c.STYLE_BODY
+            create_body([
+                dcc.Graph(id="plot_evo", config=PLOT_CONFIG)
+            ]), id="body", style=c.STYLE_BODY
         ),
     ])
